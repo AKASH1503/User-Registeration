b77a5ecafff39df152b89ea5275a15f4455f3b55
AKASH1503 <rasalakash8@gmail.com>
2020-01-20 20:50:02 +0530

    Initial Update

---
+++ UserReg/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UserReg.Startup))]
+namespace UserReg
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
